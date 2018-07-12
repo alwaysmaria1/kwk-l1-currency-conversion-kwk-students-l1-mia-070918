5da998cc7b4fe8f72e5378b77e152c70d1b9f866
alwaysmaria1 <alwaysmaria1@gmail.com>
2018-07-12 13:43:08 +0000

    automatically backed up by learn

--- currency.rb
+++ currency.rb
@@ -1,1 +1,42 @@
-# Write your cod here
+# Write your cod 
+puts "How many dollars do you have?"
+us_dollar= gets.strip.to_f 
+
+def usd_to_eur(us_dollar)
+  #convert us into euros
+   euros= us_dollar*0.86
+   puts euros
+end
+puts usd_to_eur(us_dollar)
+def usd_to_eur(us_dollar)
+  #convert us into euros
+   euros= us_dollar*0.86
+   puts euros
+end
+puts usd_to_eur(us_dollar)def usd_to_eur(us_dollar)
+  #convert us into euros
+   euros= us_dollar*0.86
+   puts euros
+end
+puts usd_to_eur(us_dollar)def usd_to_eur(us_dollar)
+  #convert us into euros
+   euros= us_dollar*0.86
+   puts euros
+end
+puts usd_to_eur(us_dollar)def usd_to_eur(us_dollar)
+  #convert us into euros
+   euros= us_dollar*0.86
+   puts euros
+end
+puts usd_to_eur(us_dollar)def usd_to_eur(us_dollar)
+  #convert us into euros
+   euros= us_dollar*0.86
+   puts euros
+end
+puts usd_to_eur(us_dollar)def usd_to_eur(us_dollar)
+  #convert us into euros
+   euros= us_dollar*0.86
+   puts euros
+end
+puts usd_to_eur(us_dollar)
+
